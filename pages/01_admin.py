697761dd23bc74245341eb3b02f77ac0c0f1d517
jamesrlathrop <james@24-7-help.com>
2026-02-20 09:40:48 -0600

    Update 01_admin.py

--- pages/01_admin.py
+++ pages/01_admin.py
@@ -3,21 +3,8 @@ from btm_admin import require_admin_key
 
 
 def main():
-    st.title("Admin Panel")
     require_admin_key()
-
-    st.markdown(
-        """
-        ## Admin tools (coming soon)
-        Once enabled, this panel will help you:
-
-        - generate new access codes
-        - add/remove codes
-        - view usage stats
-
-        For now, this page simply proves admin authentication works.
-        """
-    )
+    st.write("Admin OK")
 
 
 if __name__ == "__main__":
