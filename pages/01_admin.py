39d531c80f3af5db36d2fe0545d3d7e51c03ce99
jamesrlathrop <james@24-7-help.com>
2026-02-25 21:04:12 -0600

    Update 01_admin.py

--- pages/01_admin.py
+++ pages/01_admin.py
@@ -1,11 +1,6 @@
 import streamlit as st
 from btm_admin import require_admin_key
 
-
-def main():
-    require_admin_key()
-    st.write("Admin OK")
-
-
-if __name__ == "__main__":
-    main()
+require_admin_key()
+st.title("Admin OK")
+st.caption("Use the sidebar: generate codes / manage codes")
