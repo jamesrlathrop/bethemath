41f5dc6cae5f7f310dfa1c771723e7402aed3080
jamesrlathrop <james@24-7-help.com>
2026-02-25 21:03:37 -0600

    Update 02_codes.py

--- pages/02_codes.py
+++ pages/02_codes.py
@@ -1,6 +1,10 @@
+
 import streamlit as st
 from btm_admin import require_admin_key
 
 require_admin_key()
+
 st.title("Admin: Access Codes")
-st.write("Admin code tools will be built next.")
+st.write("Use the sidebar pages:")
+st.markdown("- **generate codes**: create new customer codes")
+st.markdown("- **manage codes**: view/revoke codes")
